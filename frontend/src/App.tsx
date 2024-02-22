0e43c6849244efc39ed149f4722305a40ebd6048
olegus199 <okremnyov@yandex.ru>
2024-02-22 21:58:28 +0300

    add init site loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,39 @@
-import "./App.scss";
+import { useEffect, useState } from "react";
+import styles from "./App.module.scss";
 import About from "./Components/About";
 import Advantages from "./Components/Advantages";
 import Hero from "./Components/Hero";
 import NavBar from "./Components/NavBar";
 
 function App() {
+  const [loading, set_loading] = useState(true);
+  const [loader_visible, set_loader_visible] = useState(true);
+  console.log(loading);
+
+  const handle_loading = () => {
+    setTimeout(() => {
+      set_loading(false);
+    }, 1300);
+  };
+
+  useEffect(() => {
+    if (!loading) {
+      set_loader_visible(false);
+    }
+  }, [loading]);
   return (
     <>
-      <NavBar />
-      <Hero />
-      <About />
-      <Advantages />
+      {loader_visible && (
+        <div className={styles.loader_container}>
+          <div className={styles.loader}></div>
+        </div>
+      )}
+      <div onLoad={handle_loading}>
+        <NavBar />
+        <Hero />
+        <About />
+        <Advantages />
+      </div>
     </>
   );
 }
