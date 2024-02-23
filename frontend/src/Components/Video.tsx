a7e3fee20719e916be6db1d0f9743c0c3a2c095f
olegus199 <okremnyov@yandex.ru>
2024-02-23 07:34:06 +0300

    set video component

--- frontend/src/Components/Video.tsx
+++ frontend/src/Components/Video.tsx
@@ -2,11 +2,28 @@ import ReactPlayer from "react-player";
 import styles from "./Video.module.scss";
 import { FC } from "react";
 
+const video_url = "https://www.youtube.com/watch?v=e1hc3O213zg";
+
 const Video: FC = () => {
   return (
     <section className={styles.video_section}>
       <div className={styles.video_wrapper}>
-        <ReactPlayer url="https://www.youtube.com/watch?v=e1hc3O213zg" />
+        <ReactPlayer
+          className={styles.react_player}
+          url={video_url}
+          width="100%"
+          height="100%"
+          controls
+          config={{
+            youtube: {
+              playerVars: {
+                autoplay: 1,
+                mute: 1,
+                loop: 1,
+              },
+            },
+          }}
+        />
       </div>
     </section>
   );
