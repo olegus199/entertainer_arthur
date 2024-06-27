624149f82e899597e200325566d7370845892c2f
olegus199 <okremnyov@yandex.ru>
2024-06-27 19:47:59 +0300

    Update types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,1 +1,15 @@
 export type SectionNames = "hero" | "about" | "gallery" | "contacts";
+
+export interface SectionRefs {
+  hero_ref: React.RefObject<HTMLDivElement>;
+  about_ref: React.RefObject<HTMLDivElement>;
+  gallery_ref: React.RefObject<HTMLDivElement>;
+  contacts_ref: React.RefObject<HTMLDivElement>;
+}
+
+export interface SectionHeights {
+  hero: number;
+  about: number;
+  gallery: number;
+  contacts: number;
+}
