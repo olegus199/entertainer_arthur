8cdf5b94d95e5bf08a1eda27177d739e4496e531
olegus199 <okremnyov@yandex.ru>
2024-06-27 19:48:39 +0300

    Update helpers

--- frontend/src/helpres.tsx
+++ frontend/src/helpres.tsx
@@ -1,3 +1,5 @@
+import { SectionHeights, SectionNames } from "./types";
+
 type InputRef = React.RefObject<HTMLInputElement>;
 
 export const handle_enter_key_down = (
@@ -25,5 +27,30 @@ export function handle_phone_click() {
     window.location.href = `tel:${phone_number}`;
   } else {
     window.location.href = `sms:${phone_number}`;
+  }
+}
+
+export function handle_scroll_to_section(
+  section_name: SectionNames,
+  heights: SectionHeights,
+  y_offset: number
+) {
+  let y: number;
+  switch (section_name) {
+    case "hero":
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      break;
+    case "about":
+      y = heights.about + window.scrollY - y_offset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+      break;
+    case "gallery":
+      y = heights.gallery + window.scrollY - y_offset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+      break;
+    case "contacts":
+      y = heights.contacts + window.scrollY - y_offset;
+      window.scrollTo({ top: y, behavior: "smooth" });
+      break;
   }
 }
